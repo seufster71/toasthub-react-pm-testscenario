02f4dc6fbd14a6a09d954c48d029059f978ec672
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-06-07 12:47:12 -0400

    pm buildout

--- testscenario-container.js
+++ testscenario-container.js
@@ -177,7 +177,7 @@ class PMTestScenarioContainer extends Component {
 				id = item.id;
 			}
 			fuLogger.log({level:'TRACE',loc:'TestScenarioContainer::onModify',msg:"test"+id});
-			this.props.actions.modifyItem(id);
+			this.props.actions.modifyItem({id,appPrefs:this.props.appPrefs});
 		};
 	}
 	
